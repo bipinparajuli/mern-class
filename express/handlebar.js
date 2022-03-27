f896c6551f657f5b2d710fbeee6b9270ac84b54b
bipinparajuli <bipinprjl@gmail.com>
2022-03-27 18:09:06 +0545

    helpers

--- express/handlebar.js
+++ express/handlebar.js
@@ -1,18 +1,38 @@
+const express =  require("express")
 const app =  require("express")();
 const {engine} = require("express-handlebars");
 
-app.engine("handlebars",engine({
+app.use(express.static("public"));
 
+app.engine("hbs",engine({
+  layoutsDir:"views/layoutsss",
+  defaultLayout:"abc",
+  partialsDir:"views/abc",
+  extname:"hbs"
 }));
 
-app.set("view engine","handlebars");
+app.set("view engine","hbs");
+
 
 app.get("/",(req,res)=>{
-  res.render("index")  
+  res.render("index",{
+  title:"Home",
+  name:"Santosh",
+  isAdmin:true,
+  arr:[1,2,3,4,5]
+})  
+
 })
 
 app.get("/about",(req,res)=>{
-    res.render("about")  
+    res.render("about",{
+      title:"About",
+      name:{
+        fname:"Someone",
+        lname:"Sirname"
+      },
+      isAdmin:false
+    })  
   })
 
 
