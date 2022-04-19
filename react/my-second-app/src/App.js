11ceaaa8a7b548a717dfec0a624ed3366854a010
bipinparajuli <bipinprjl@gmail.com>
2022-04-19 21:54:15 +0545

    useeffect

--- react/my-second-app/src/App.js
+++ react/my-second-app/src/App.js
@@ -1,9 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 
 function App() {
-const [state,setState] = useState(true) //declaring state
+const [state,setState] = useState("") //declaring state
 const [arr,setarr] = useState([])
 
 function handleChange(event){ // function to update current state
@@ -18,8 +18,33 @@ function handleChange(event){ // function to update current state
   // },3000)
 }
 
+function handleClick(){
+  // console.log(arr);
+  // setarr([...arr,state])
+  // setState("Hello")
+
+  fetch('https://jsonplaceholder.typicode.com/todos/1')
+  .then(response => response.json())
+  .then(json => setState(json))
+}
+
+// useEffect(()=>{
+//   console.log("HII")
+// })
+
+// useEffect(()=>{
+//   console.log("HELLO")
+// },[])
+
+useEffect(()=>{
+  console.log("I AM STATE",state)
+},[state])
+
+
+
+
 function add(){
-  setarr([...arr,state])
+  // setarr([...arr,state])
 }
 // arr = ["hello","hi"]
 return (
@@ -29,14 +54,20 @@ return (
       {/* /> */}
       {/* <h6>I'm {state}</h6> */}
       {/* {state?<button onClick={handleChange} >Login</button>:<button>Loading . . .</button>} */}
+      <h1>{state.title}</h1>
+
     <input
-    
+      // value={state}
+    onChange={(event)=>{
+      // setState(event.target.value)
+    }}
+
     />
-    <button >Add</button>
-    {arr.map(data=>(
-      <>
-      <p>{data}</p>
-      </>
+    <button onClick={handleClick} >Add</button>
+    {arr.map((data,i)=>(
+      <div key={i}>
+      <p>{data}</p><button>Delete</button>
+      </div>
     ))}
     </div>
   );
